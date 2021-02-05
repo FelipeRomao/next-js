14d247f6f5857a671f3980258efa05de8c37cf36
Felipe Romao <feliperomao.a@gmail.com>
2021-02-04 22:39:16 -0300

    Fetching page from product on Prismic

--- src/components/AddToCartModal.tsx
+++
@@ -1,3 +0,0 @@
-export default function AddToCartModal() {
-  return <p>Do you really want to add this product to the cart</p>;
-}
--- src/pages/catalog/product/[slug].tsx
+++ src/pages/catalog/product/[slug].tsx
@@ -1,22 +1,50 @@
-import { useState } from "react";
-import dynamic from "next/dynamic";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { Document } from "prismic-javascript/types/documents";
+import { useRouter } from "next/router";
+import PrismicDOM from "prismic-dom";
 
-const AddToCartModal = dynamic(() => import("@/components/AddToCartModal"), {
-  loading: () => <p>Loading...</p>,
-});
+import { client } from "@/lib/prismic";
 
-export default function Product() {
-  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
+interface ProductProps {
+  product: Document;
+}
 
-  function handleAddToCart() {
-    setIsAddToCartModalVisible(true);
+export default function Product({ product }: ProductProps) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <p>Carregando...</p>;
   }
 
   return (
     <>
-      <button onClick={handleAddToCart}>Add to cart</button>
+      <p>{PrismicDOM.RichText.asText(product.data.title)}</p>
+      <div
+        dangerouslySetInnerHTML={{
+          __html: PrismicDOM.RichText.asHtml(product.data.description),
+        }}
+      />
 
-      {isAddToCartModalVisible && <AddToCartModal />}
+      <img src={product.data.thumbnail.url} alt="" width="400" />
+
+      <p>Price: R${product.data.price}</p>
     </>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: true,
+  };
+};
+
+export const getStaticProps: GetStaticProps<ProductProps> = async (context) => {
+  const { slug } = context.params;
+  const product = await client().getByUID("product", String(slug), {});
+
+  return {
+    props: { product },
+    revalidate: 5,
+  };
+};
